189aad319987d444fa0d6bffc90c84090a73953e
Ark4ne <gui.allegret@gmail.com>
2015-07-13 00:20:39 +0200

    * Test + Force Fail

--- tests/OSTest.php
+++ tests/OSTest.php
@@ -66,6 +66,7 @@ class OSTest extends \PHPUnit_Framework_TestCase
 		$processes = OS::os()->processes('php');
 
 		$this->assertTrue(is_array($processes));
+		$this->assertEquals([], $processes);
 		$this->assertEquals(0, count($processes));
 		$this->assertEquals(count($processes), OS::os()->countProcesses('php'));
 	}
@@ -76,11 +77,12 @@ class OSTest extends \PHPUnit_Framework_TestCase
 		$cmd = new Ark4ne\Process\Command\Command('php', __DIR__ . '/command/basic.php');
 
 		$cmd->exec(true);
-		
+
 		$processes = OS::os()->processes('php');
 
 		$this->assertTrue(is_array($processes));
-		$this->assertEquals(1, count($processes));
+		$this->assertEquals([], $processes);
+		$this->assertEquals(2, count($processes));
 		$this->assertEquals(count($processes), OS::os()->countProcesses('php'));
 
 		sleep(3);
